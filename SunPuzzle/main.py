da747460b65a2a0dd284a75261863af2c86be186
JacksonTQY <97141507+JacksonTQY@users.noreply.github.com>
2023-06-15 13:17:26 +0800

    Movelist will not persist upon browser refresh any more

--- SunPuzzle/main.py
+++ SunPuzzle/main.py
@@ -19,12 +19,14 @@ def index():
 
         f_count = int(f_count)
         cur_state, f_count = sp.panel_config(cur_state, panel, f_count)
+        global movelist
         movelist.append(f"You pressed Panel {panel}")
         print(movelist)
         if cur_state == sp.ans:
             flash(f"Congratulations! You have solved the puzzle in {len(movelist)} moves!", "success")
     else:
         cur_state, f_count = sp.reset()[0], sp.reset()[1]
+        movelist = []
 
     # print(cur_state, f_count)
     return render_template("index.html", cur_state=cur_state, f_count=f_count, ans=sp.ans,
